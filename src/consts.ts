07a9531623e86879175b3aba1dc3f11cba02a4df
nczxe <165904972+nczxe@users.noreply.github.com>
2025-08-30 04:13:57 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Lu的博客";
+export const SITE_TITLE = "FR-NEXT的小站";
 export const SITE_DESCRIPTION = "欢迎来到我的博客！";
